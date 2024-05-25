250fd2cbf23a09c38b4fe89412ed8c0b8cdc0e8a
Javier <jamesdelaney888@hotmail.com>
2024-05-25 08:30:29 +0200

    same

--- src/routes/api/upload-image/+server.ts
+++ src/routes/api/upload-image/+server.ts
@@ -4,24 +4,15 @@ import { json, type RequestHandler } from '@sveltejs/kit';
 import fs from 'fs';
 import path from 'path';
 
-// const uploadDir = path.join(process.cwd(), RAILWAY_VOLUME_MOUNT_PATH);
-// const uploadDir = path.join(process.cwd(), '/uploads');
 export const POST: RequestHandler = async ({ request }) => {
 	const formData = await request.formData();
-	const lang = formData.get('lang');
 	const file = formData.get('file') as File;
 
 	console.log('🚀 ~ file:', file);
-	// console.log('🚀 ~ lang:', lang);
 	const uploadDir = RAILWAY_VOLUME_MOUNT_PATH;
-
-	// !validate syntax
-	// if (!lang || !LANGS.includes(lang)) return jsonErrGeneric(lang);
 
 	// !save file locally
 	const fileName = `primera-img${path.extname(file.name)}`;
-	// const fileName = `${Date.now()}-${path.extname(file.name)}`;
-	// const uploadPath = path.join(uploadDir, fileName);
 	const uploadPath = RAILWAY_VOLUME_MOUNT_PATH + fileName;
 
 	try {
@@ -30,7 +21,7 @@ export const POST: RequestHandler = async ({ request }) => {
 
 		const arrayBuffer = await file.arrayBuffer();
 		const buffer = Buffer.from(arrayBuffer);
-		await fs.promises.writeFile(uploadPath, buffer);
+		await fs.promises.writeFile(uploadPath, file);
 		console.log('Archivo guardado exitosamente');
 	} catch (err) {
 		console.error('Error al guardar el archivo:', err);
